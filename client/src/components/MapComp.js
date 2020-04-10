058478f4deed8860e1625b72068ddbe2306a945a
Maria Francis-Moullier <moullier@gmail.com>
2020-04-10 14:47:58 -0700

    starter map on homepage, placeholder links for login

---
+++ client/src/components/MapComp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MapComp extends Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>Map placeholder</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default MapComp;
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+let session = require("express-session");
+// Requiring passport as we've configured it
+let passport = require("./config/passport");
+
 const app = express();
+let db = require("./models");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring our routes
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
+
 
 // Send every request to the React app
 // Define any API routes before this runs
