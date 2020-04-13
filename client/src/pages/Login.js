34d8b9f2bea2f854f1b4070348198f496eded409
Jake Kravas <jakekravas7@gmail.com>
2020-04-13 15:35:34 -0700

    updated layout of main page and added some styling to login and signup pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,30 +1,71 @@
-import React, { Component } from "react";
+import React from 'react'
+import {Link} from "react-router-dom"
+import $ from "jquery";
 
-class Login extends Component {
-    render() {
-      return (
-        <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-md-offset-3">
-            <h2>Login Form</h2>
-            <form className="login">
-              <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="email-input" placeholder="Email" />
-              </div>
-              <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="password-input" placeholder="Password" />
-              </div>
-              <button type="submit" className="btn btn-default">Login</button>
-            </form>
-            <br />
-            <p>Or sign up <a href="/">here</a></p>
+const Login = () => {
+  
+  const toggleEye = () => {
+    $("#eye").toggleClass("fa-eye-slash");
+    $("#eye").toggleClass("fa-eye");
+    if ($("#eye").hasClass("fa-eye")){
+      $("#password").attr("type", "text");
+    } else {
+      $("#password").attr("type", "password");
+    }
+  }
+
+  const onUserSubmit = e => {
+    e.preventDefault();
+    // Validation will be here
+  }
+
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-md-5 text-center mx-auto">
+          <div className="card" id="su-card">
+            <div className="card-body">
+              <h4 id="signup-header" className="pb-2">Log In</h4>
+              {/* <form onSubmit={onUserSubmit} autoComplete="off"> */}
+              <form onSubmit={onUserSubmit}>
+
+                {/* EMAIL */}
+                <div className="input-group my-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text pre-inp">
+                      <i class="far fa-envelope"></i>
+                    </span>
+                  </div>
+                  <input type="text" className="form-control su-inp" id="email" placeholder="Email"/>
+                </div>
+
+                {/* PASSWORD */}
+                <div className="input-group mb-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text pre-inp">
+                      <i class="fas fa-key"></i>
+                    </span>
+                  </div>
+                  {/* <i class="fas fa-eye-slash" id="eye" onClick={toggleEye}></i> */}
+                  <input type="password" className="form-control su-inp" id="password" placeholder="Password" autoComplete="off"/>
+                  <div className="input-group-append">
+                    <span className="input-group-text" id="eye-cont">
+                      <i class="fas fa-eye-slash" id="eye" onClick={toggleEye}></i>
+                    </span>
+                  </div>
+                </div>
+
+                <p className="text-muted">
+                  Don't have an account? Sign up <Link to={"/signup2"}>here!</Link>
+                </p>
+                <button type="submit" className="btn form-btn">Log In</button>         
+              </form>
+            </div>
           </div>
         </div>
       </div>
-      );
-    }
-  }
-  
-  export default Login;
+    </div>
+  )
+}
+
+export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,34 +1,68 @@
-import React, { Component } from "react";
+import React from 'react'
+import {Link} from "react-router-dom"
+import $ from "jquery";
 
-class Signup extends Component {
-    render() {
-      return (
-        <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-md-offset-3">
-            <h2>Sign Up Form</h2>
-            <form className="signup">
-              <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="email-input" placeholder="Email" />
-              </div>
-              <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="password-input" placeholder="Password" />
-              </div>
-              {/* <div style="display: none" id="alert" className="alert alert-danger" role="alert">
-                <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                <span className="sr-only">Error:</span> <span className="msg"></span>
-              </div> */}
-              <button type="submit" className="btn btn-default">Sign Up</button>
-            </form>
-            <br />
-            <p>Or log in <a href="/login">here</a></p>
+const Signup = () => {
+
+  const onUserSubmit = e => {
+    e.preventDefault();
+    let passwordVal = $("#password").val();
+    let confirmPasswordVal = $("#confirmPassword").val();
+    if ( passwordVal !== confirmPasswordVal){
+      alert("Passwords do not match")
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-md-5 text-center mx-auto">
+          <div className="card" id="su-card">
+            <div className="card-body">
+              <h4 id="signup-header" className="pb-2">Sign Up</h4>
+              <form onSubmit={onUserSubmit}>
+
+                {/* EMAIL */}
+                <div className="input-group my-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text pre-inp">
+                      <i class="far fa-envelope"></i>
+                    </span>
+                  </div>
+                  <input type="text" className="form-control su-inp" id="email" placeholder="Email"/>
+                </div>
+
+                {/* PASSWORD */}
+                <div className="input-group mb-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text pre-inp">
+                      <i class="fas fa-key"></i>
+                    </span>
+                  </div>
+                  <input type="password" className="form-control su-inp" id="password" placeholder="Password"/>
+                </div>
+
+                {/* CONFIRM PASSWORD */}
+                <div className="input-group mb-4">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text pre-inp">
+                      <i class="fas fa-shield-alt"></i>
+                    </span>
+                  </div>
+                  <input type="password" className="form-control su-inp" id="confirmPassword" placeholder="Confirm Password"/>
+                </div>
+
+                <p className="text-muted">
+                  Already have an account? Sign in <Link to={"/login2"}>here</Link>
+                </p>
+                <button type="submit" className="btn form-btn">Register</button>         
+              </form>
+            </div>
           </div>
         </div>
       </div>
-      );
-    }
-  }
-  
-  export default Signup;
+    </div>
+  )
+}
+
+export default Signup;
