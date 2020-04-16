51f5c56e3baa4e56e238860f2cd57029beff6821
Maria Francis-Moullier <moullier@gmail.com>
2020-04-15 18:42:05 -0700

    Signup page pushes to database, Login page still in progress

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,29 +1,92 @@
 import React, { Component } from "react";
+import Axios from "axios";
+
+
 
 class Signup extends Component {
-    render() {
+
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  sayHello() {
+    alert('Hello!');
+  }
+
+  // handleChange(event) {
+  //   console.log("handleChange is called");
+  //   console.log("e.t = ");
+  //   console.log(event.target);
+  //   this.setState({
+  //     username: event.target.username,
+  //     password: event.target.password
+  //   });
+  // }
+
+  handleChange(evt) {
+    const value = evt.target.value;
+    this.setState({
+      ...this.state,
+      [evt.target.name]: value
+    });
+  }
+
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.username);
+    console.log(this.state.password);
+    
+    Axios.post("/api/signup", {
+      email: this.state.username,
+      password: this.state.password
+    })
+      .then(function(data) {
+        console.log("data: ");
+        console.log(data);
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+  }
+  
+
+  render() {
       return (
         <div className="container">
         <div className="row">
           <div className="col-md-6 col-md-offset-3">
             <h2>Sign Up Form</h2>
-            <form className="signup">
+            <form className="signup" onSubmit={this.handleSubmit}>
               <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="email-input" placeholder="Email" />
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" name="username" value={this.state.username} onChange={this.handleChange} placeholder="Email"/>
               </div>
               <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="password-input" placeholder="Password" />
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password"/>
+                {/* <input type="password" className="form-control" id="password-input" placeholder="Password" /> */}
               </div>
               {/* <div style="display: none" id="alert" className="alert alert-danger" role="alert">
                 <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
                 <span className="sr-only">Error:</span> <span className="msg"></span>
               </div> */}
-              <button type="submit" className="btn btn-default">Sign Up</button>
+              <input type="submit" value="Create Account" />
             </form>
             <br />
             <p>Or log in <a href="/login">here</a></p>
+
+            <button onClick={this.sayHello}>
+              Click me!
+            </button>
+
           </div>
         </div>
       </div>
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 let bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
-  let Member = sequelize.define("Member", {
+  let User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
     email: {
       type: DataTypes.STRING,
@@ -19,13 +19,13 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  Member.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  Member.addHook("beforeCreate", function(member) {
-    member.password = bcrypt.hashSync(member.password, bcrypt.genSaltSync(10), null);
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
-  return Member;
+  return User;
 };
