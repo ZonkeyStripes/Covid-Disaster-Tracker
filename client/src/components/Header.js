1520b6d89366b73a4c7969944dc0a7e1866483eb
Jake Kravas <jakekravas7@gmail.com>
2020-04-24 20:11:02 -0700

    fixed dropdown links on header for mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,10 +20,10 @@ const Header = () => {
               <NavLink exact={true} id="news-link" className="header-link" activeClassName="header-link-active" to={"/news"}>News</NavLink>
             </ReactBootStrap.Nav.Link>
             <ReactBootStrap.Nav.Link>
-              <NavLink exact={true} className="header-link" activeClassName="header-link-active" to={"/disasters"}>Disasters</NavLink>
+              <NavLink exact={true} id="disasters-link" className="header-link" activeClassName="header-link-active" to={"/disasters"}>Disasters</NavLink>
             </ReactBootStrap.Nav.Link>
             <ReactBootStrap.Nav.Link>
-              <NavLink exact={true} className="header-link" activeClassName="header-link-active" to={"/kit"}>Disaster Kit</NavLink>
+              <NavLink exact={true} id="disaster-kit-link" className="header-link" activeClassName="header-link-active" to={"/kit"}>Disaster Kit</NavLink>
             </ReactBootStrap.Nav.Link>
           </ReactBootStrap.Nav>
         <ReactBootStrap.Nav className="ml-auto">
